c5f1b8ab68b5e2cc0d4164e85f7d779ddd752e15
yangyuegly <yangyuegly@github.com>
2020-11-30 15:26:52 -0500

    modified main to include reading video

--- main.py
+++ main.py
@@ -6,34 +6,54 @@ CS 1290 Computational Photography, Brown U.
 Usage-
 
 To run tracking on green pixels:
-  python main.py
+  python main.py --s [source video file path]
 
 
 
 To run tracking on an object using YOLO
-  python main.py --yolo (or -y)
+  python main.py --yolo (or -y) --s [source video file path]
 
 '''
 import numpy as np
 from tracking import track_green
 from tracking import track_yolo
 
+
 def lightpainting(method):
     if method == "green":
         # need to change main function to take in an image, for now, it is hard coded
         img = "img_name.jpeg"
         points = track_green(img)
-    else if method == "yolo":
+    elif method == "yolo":
         img = "img_name.jpeg"
         points = track_yolo(img)
+
+
+def parse(source, method):
+    """
+    reads in the video input and call tracking on each frame 
+    """
+    cap = cv2.VideoCapture(source)
+    success, image = cap.read()
+
+    count = 0
+    while success:
+        cv2.imwrite("frame%d.jpg" % count, image)  # save frame as JPEG file
+        lightpainting(method)
+        success, image = cap.read()
+        print('Read a new frame: ', success)
+        count += 1
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="CS1290 Lightpainting")
     parser.add_argument("-y", "--yolo", help="Indicates to track using YOLO. Default is green.",
-        action="store_true")
+                        action="store_true")
+    parser.add_argument(
+        "-s", "--source", help="Name of the source video with extension")
     args = vars(parser.parse_args())
     if args["yolo"]:
         method = "yolo"
     else:
         method = "green"
-    lightpainting(method)
+    parse(source, method)
--- tracking.py
+++ tracking.py
@@ -13,17 +13,21 @@ To run tracking on an object using YOLO, use track_yolo()
 
 import numpy as np
 
+
 def track_green(img):
     '''
     Returns the positions (x,y) of green pixels within the input image img
     '''
-    points = numpy.zeros((1,1))
+
+    mask =
+    points = numpy.zeros((1, 1))
     return points
+
 
 def track_yolo(img):
     '''
     Returns the positions (x,y) of the bounding box around the subject
     within the input image img
     '''
-    points = numpy.zeros((1,1))
+    points = numpy.zeros((1, 1))
     return points
